227258c34ee4023bf72e20b165e94ec2b59b963e
Chris Ward <cw@yellowdave.co.uk>
2023-11-04 17:57:09 +0000

    added VexFlow libs

--- src/app/features/components/vexflow/vexflow.component.ts
+++ src/app/features/components/vexflow/vexflow.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import * as vexflow from 'vexflow';
 
 @Component({
   selector: 'app-vexflow',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './vexflow.component.html',
-  styleUrls: ['./vexflow.component.scss']
+  styleUrls: ['./vexflow.component.scss'],
 })
 export class VexflowComponent {
-
+  ngOnInit() {
+    // const VF = vexflow.Flow;
+    // var theDiv = document.getElementById('boo');
+    // var renderer = new VF.Renderer(
+    //   theDiv as HTMLElement,
+    //   VF.Renderer.Backends.SVG
+    // );
+  }
 }
