1d42be87ce912adb75adec0557bc35b097f12b78
Chris Ward <cw@yellowdave.co.uk>
2023-11-05 10:58:48 +0000

    Add test example for EasyScore

--- src/app/features/components/vexflow/vexflow.component.ts
+++ src/app/features/components/vexflow/vexflow.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import * as vexflow from 'vexflow';
 import { Vex } from 'vexflow';
+import { Factory } from 'vexflow';
 
 @Component({
   selector: 'app-vexflow',
@@ -21,7 +22,33 @@ export class VexflowComponent implements OnInit {
   // );
 
   ngOnInit() {
-    var div = document.getElementById('vexflow-player');
+    this.easyScore();
+    this.nativeAPI();
+  }
+
+  easyScore() {
+    const vf = new Factory({
+      renderer: { elementId: 'easy-score', width: 500, height: 200 },
+    });
+
+    const score = vf.EasyScore();
+    const system = vf.System();
+
+    system
+      .addStave({
+        voices: [
+          score.voice(score.notes('C#5/q, B4, A4, G#4', { stem: 'up' })),
+          score.voice(score.notes('C#4/h, C#4', { stem: 'down' })),
+        ],
+      })
+      .addClef('treble')
+      .addTimeSignature('4/4');
+
+    vf.draw();
+  }
+
+  nativeAPI() {
+    var div = document.getElementById('native-api');
     this.VF = Vex.Flow;
 
     var renderer = new this.VF.Renderer(div, this.VF.Renderer.Backends.SVG);
