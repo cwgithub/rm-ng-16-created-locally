1ccdc3f654956ed539f38f29aefce67d2d3ab737
Chris Ward <cw@yellowdave.co.uk>
2023-11-05 15:21:00 +0000

    Beaming 2

--- src/app/features/components/vexflow/vexflow.component.ts
+++ src/app/features/components/vexflow/vexflow.component.ts
@@ -38,6 +38,7 @@ export class VexflowComponent implements OnInit {
     // this.tutorialStep2();
     this.tutorialStep3();
     this.tutorialStep4();
+    this.tutorialStep5();
   }
 
   easyScore() {
@@ -66,7 +67,7 @@ export class VexflowComponent implements OnInit {
     this.VF = Vex.Flow;
 
     const renderer = new this.VF.Renderer(div, this.VF.Renderer.Backends.SVG);
-    renderer.resize(500, 500);
+    renderer.resize(500, 200);
 
     const context = renderer.getContext();
     context.setFont('Arial', 10, '').setBackgroundFillStyle('#eed');
@@ -89,7 +90,7 @@ export class VexflowComponent implements OnInit {
       this.renderer = new Renderer(div, Renderer.Backends.SVG);
 
       // Configure the rendering context.
-      this.renderer.resize(500, 200);
+      this.renderer.resize(500, 150);
       this.context = this.renderer.getContext();
     }
   }
@@ -187,7 +188,7 @@ export class VexflowComponent implements OnInit {
     const renderer = new Renderer(div, Renderer.Backends.SVG);
 
     // Configure the rendering context.
-    renderer.resize(500, 200);
+    renderer.resize(500, 150);
     const context = renderer.getContext();
 
     // Create a stave of width 400 at position 10, 40 on the canvas.
@@ -274,6 +275,61 @@ export class VexflowComponent implements OnInit {
     function dotted(staveNote: StaveNote) {
       Dot.buildAndAttach([staveNote]);
       return staveNote;
+    }
+  }
+
+  tutorialStep5() {
+    // Create an SVG renderer and attach it to the DIV element named "boo".
+    const div = document.getElementById('tutorial-step-5') as HTMLDivElement;
+    const renderer = new Renderer(div, Renderer.Backends.SVG);
+
+    // Configure the rendering context.
+    renderer.resize(500, 150);
+    const context = renderer.getContext();
+
+    // Create a stave of width 400 at position 10, 40 on the canvas.
+    const stave = new Stave(10, 40, 400);
+
+    // Add a clef and time signature.
+    stave.addClef('treble').addTimeSignature('4/4');
+
+    // Connect it to the rendering context and draw!
+    stave.setContext(context).draw();
+
+    const notes = [
+      dotted(
+        new StaveNote({ keys: ['e##/5'], duration: '8d' }).addModifier(
+          new Accidental('##')
+        )
+      ),
+      new StaveNote({ keys: ['b/4'], duration: '16' }).addModifier(
+        new Accidental('b')
+      ),
+      new StaveNote({ keys: ['c/4'], duration: '8' }),
+      new StaveNote({ keys: ['d/4'], duration: '16' }),
+      new StaveNote({ keys: ['e/4'], duration: '16' }).addModifier(
+        new Accidental('b')
+      ),
+      new StaveNote({ keys: ['d/4'], duration: '16' }),
+      new StaveNote({ keys: ['e/4'], duration: '16' }).addModifier(
+        new Accidental('#')
+      ),
+      new StaveNote({ keys: ['g/4'], duration: '32' }),
+      new StaveNote({ keys: ['a/4'], duration: '32' }),
+      new StaveNote({ keys: ['g/4'], duration: '16' }),
+      new StaveNote({ keys: ['d/4'], duration: 'q' }),
+    ];
+
+    const beams = Beam.generateBeams(notes);
+    Formatter.FormatAndDraw(context, stave, notes);
+    beams.forEach((b) => {
+      b.setContext(context).draw();
+    });
+
+    // A helper function to add a dot to a note.
+    function dotted(note: StaveNote) {
+      Dot.buildAndAttach([note]);
+      return note;
     }
   }
 }
