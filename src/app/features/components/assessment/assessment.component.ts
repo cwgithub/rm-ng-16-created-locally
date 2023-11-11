a61874227b99dc62a7a21dc16ca9acdd12ae9670
Chris Ward <cw@yellowdave.co.uk>
2023-11-11 15:17:20 +0000

    add option button radio controls

--- src/app/features/components/assessment/assessment.component.ts
+++ src/app/features/components/assessment/assessment.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-assessment',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, FormsModule, HttpClientModule, MatRadioModule],
   templateUrl: './assessment.component.html',
   styleUrls: ['./assessment.component.scss'],
 })
@@ -14,6 +16,9 @@ export class AssessmentComponent {
   static AssessmentFileName = 'assessment.json';
 
   assessment?: any;
+
+  optionGroup?: string;
+
   private _assessmentName = 'grade5';
 
   constructor(private _httpClient: HttpClient) {
