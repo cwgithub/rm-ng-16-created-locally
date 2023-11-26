0b046ab8d59e9240a2e5cb11587590888fae70fd
Chris Ward <cw@yellowdave.co.uk>
2023-11-26 15:45:40 +0000

    drag and drop trial if NgxDragDrop Revert to using cdk drag drop Identified the use of event.event.offsetX and offsetY to work out where the drag release is placed

--- src/app/features/gizmos/components/drag-and-drop/drag-and-drop.component.ts
+++ src/app/features/gizmos/components/drag-and-drop/drag-and-drop.component.ts
@@ -1,14 +1,65 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CdkDrag, CdkDropList, CdkDropListGroup } from '@angular/cdk/drag-drop';
+// import { DndDropEvent, DndModule } from 'ngx-drag-drop';
+
+// export type DndDragImageOffsetFunction = (
+//   event: DragEvent,
+//   dragImage: Element
+// ) => { x: number; y: number };
 
 @Component({
   selector: 'app-drag-and-drop',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    // DndModule,
+    CdkDrag,
+    CdkDropListGroup,
+    CdkDropList,
+  ],
   templateUrl: './drag-and-drop.component.html',
   styleUrls: ['./drag-and-drop.component.scss'],
 })
 export class DragAndDropComponent {
   dragImage = 'assets/tests/gizmos/drag-and-drop-barline.png';
   dropImage = 'assets/tests/gizmos/drag-and-drop-stave.png';
+
+  result = 'Incorrect';
+
+  cdkDragMoved(event: any) {
+    console.log(`${event.event.x}, ${event.event.y}`);
+  }
+
+  cdkDragStarted(event: any) {
+    console.log(event);
+  }
+
+  cdkDragReleased(event: any) {
+    if (event.event.x >= 400 && event.event.x <= 445) {
+      this.result = 'Correct';
+    } else {
+      this.result = 'Incorrect';
+    }
+  }
+
+  cdkDragDropped(event: any) {
+    console.log(event);
+  }
+
+  cdkDropListDropped(event: any) {
+    console.log(event);
+  }
+
+  cdkDropListEntered(event: any) {
+    console.log(event);
+  }
+
+  cdkDropListExited(event: any) {
+    console.log(event);
+  }
+
+  onDrop(event: any) {
+    console.log(event);
+  }
 }
