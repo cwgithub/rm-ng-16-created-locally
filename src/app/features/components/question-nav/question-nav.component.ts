46e9d0a66de3f83347ca40a04d31876539ff9372
Chris Ward <cw@yellowdave.co.uk>
2023-12-17 09:40:41 +0000

    indicates which questions the user has answered

--- src/app/features/components/question-nav/question-nav.component.ts
+++ src/app/features/components/question-nav/question-nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { SectionSpec } from '../../../core/models/interfaces';
+import { AssessmentSpec, SectionSpec } from '../../../core/models/interfaces';
+import { AnswerService } from 'src/app/core/services/answer.service';
 
 @Component({
   standalone: true,
@@ -9,6 +10,31 @@ import { SectionSpec } from '../../../core/models/interfaces';
   styleUrl: './question-nav.component.scss',
 })
 export class QuestionNavComponent {
+  @Input() userId?: string;
+  @Input() assessmentId?: string;
   @Input() sectionSpec?: SectionSpec;
   @Output() questionClickEvent = new EventEmitter<number>();
+
+  constructor(private _answerService: AnswerService) {}
+
+  isAnswered(questionNumber: number): boolean {
+    if (!this.userId) {
+      throw new Error('No user id set');
+    }
+    if (!this.assessmentId) {
+      throw new Error('No assessment id set');
+    }
+    if (!this.sectionSpec) {
+      throw new Error('No active section set');
+    }
+
+    const userAnswer = this._answerService.getAnswer(
+      this.userId,
+      this.assessmentId,
+      this.sectionSpec?.sectionName,
+      questionNumber
+    );
+
+    return userAnswer !== undefined;
+  }
 }
